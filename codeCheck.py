f3ca60675c9ba94bd56514e6841a519ae0de548d
cb-kamalraj <kamalraj.s@chargebee.com>
2022-07-19 16:58:51 +0530

    updated keys

--- codeCheck.py
+++ codeCheck.py
@@ -1,5 +1,10 @@
 import os
-input1 = os.getenv("validation")
-input2 = os.getenv("email_recipients")
+input1 = os.getenv("email_recipients")
+input2 = os.getenv("Validations")
 Dropdown = os.getenv("Dropdown")
 Dropdown2 = os.getenv("dropdown2")
+
+print(input1)
+print(input2)
+print(input1)
+print(input1)
